8005887ff902b31d7afcb2d005dd540f87233d7d
zhangshuai <1234.com>
2017-12-15 11:06:16 +0800

    修改"个人中心"功能列表，基本完成业务设置功能(待tokenid解决)，添加了"我的账户"二级界面

---
+++ app/src/main/java/com/yjm/doctor/ui/AccountinfoActivity.java
@@ -0,0 +1,101 @@
+package com.yjm.doctor.ui;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.yjm.doctor.R;
+import com.yjm.doctor.ui.base.BaseActivity;
+import com.yjm.doctor.ui.view.layout.ListLayoutAdapter;
+import com.yjm.doctor.ui.view.layout.ListLayoutModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AccountinfoActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.img_operation)
+    ImageView mImgOperation;
+
+    @BindView(R.id.toolicon)
+    RelativeLayout mToolicon;
+
+    @BindView(R.id.tooltitle)
+    TextView mTooltitle;
+
+    @BindView(R.id.toolfinish)
+    TextView mToolfinish;
+
+    private ListView mListviewTop,mListviewBottom;
+    private List<ListLayoutModel> modelListTop=null,modelListBottom;
+    private ListLayoutAdapter mLayoutAdapterTop,mLayoutAdapterBottom;
+
+
+    @Override
+    public int initView() { return R.layout.activity_accountinfo; }
+
+    //返回上一级界面
+    @OnClick(R.id.img_operation)
+    public void onViewClicked() {
+        this.finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mListviewTop=(ListView)findViewById(R.id.include_top).findViewById(R.id.listview_layout);
+        mListviewTop=(ListView)findViewById(R.id.include_bottom).findViewById(R.id.listview_layout);
+
+        setSupportActionBar(mToolbar);
+        mTooltitle.setText(R.string.account_info);
+        mToolicon.setVisibility(View.GONE);
+        mToolfinish.setVisibility(View.GONE);
+
+        modelListTop=new ArrayList<ListLayoutModel>();
+        modelListTop.add(new ListLayoutModel(R.string.name,"",R.drawable.comein));
+        modelListTop.add(new ListLayoutModel(R.string.id_card,"",R.drawable.comein));
+        mLayoutAdapterTop=new ListLayoutAdapter(this,modelListTop);
+        mListviewTop.setAdapter(mLayoutAdapterTop);
+
+
+        modelListBottom=new ArrayList<ListLayoutModel>();
+        modelListBottom.add(new ListLayoutModel(R.string.alipay_account,"",R.drawable.comein));
+        modelListBottom.add(new ListLayoutModel(R.string.wechatpay_account,"",R.drawable.comein));
+        mLayoutAdapterBottom=new ListLayoutAdapter(this,modelListBottom);
+        mListviewBottom.setAdapter(mLayoutAdapterBottom);
+
+        //顶部
+        mLayoutAdapterTop.setOnListItemOnClickListener(new ListLayoutAdapter.OnListItemOnClickListener() {
+            @Override
+            public void OnItemClick(int position, ListLayoutModel model) {
+
+            }
+        });
+
+        mLayoutAdapterBottom.setOnListItemOnClickListener(new ListLayoutAdapter.OnListItemOnClickListener() {
+            @Override
+            public void OnItemClick(int position, ListLayoutModel model) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void finishButton() {
+
+    }
+
+}
